74974ff2c4293ceec48d005026ce166ee389999a
Claire <cmelville@lcs.on.ca>
2021-11-29 14:25:56 -0500

    Initial Commit

---
+++ BrainStorm/BrainStormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainStormApp.swift
+//  BrainStorm
+//
+//  Created by Claire Melville on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct BrainStormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
